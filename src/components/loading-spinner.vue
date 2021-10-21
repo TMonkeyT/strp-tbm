865ced2f062a6bf46996ad69d98d12521a4b9186
TMonkeyT <github@tmonkeyt.dev>
2021-10-21 15:49:00 +0100

    Add spinner

---
+++ src/components/loading-spinner.vue
@@ -0,0 +1,14 @@
+<template>
+    <div :class="'spinner-border ' + 'text-' + type" style="width: 3rem; height: 3rem;" role="status">
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["type"]
+}
+</script>
+
+<style>
+
+</style>
